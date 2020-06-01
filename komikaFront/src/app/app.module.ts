c9899f4251120ca429415d40aec2336dcef9900e
Alwar <alwaromoreno@gmail.com>
2020-06-01 18:13:40 +0200

    creacion componentes

--- komikaFront/src/app/app.module.ts
+++ komikaFront/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { PerfilComponent } from './perfil/perfil.component';
+import { GaleriaComponent } from './galeria/galeria.component';
+import { VisualizadorComponent } from './visualizador/visualizador.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    PerfilComponent,
+    GaleriaComponent,
+    VisualizadorComponent
   ],
   imports: [
     BrowserModule,
