0809caac7899a1e8d011125c7c34cbae073ca4d0
Alwar <alwaromoreno@gmail.com>
2020-06-02 16:11:13 +0200

    models

--- komikaFront/src/app/app-routing.module.ts
+++ komikaFront/src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PerfilComponent } from './perfil/perfil.component';
+import { GaleriaComponent } from './galeria/galeria.component';
+import { VisualizadorComponent } from './visualizador/visualizador.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomeComponent },
+  { path: 'perfil', component: PerfilComponent },
+  { path: 'galeria', component: GaleriaComponent },
+  { path: 'view', component: VisualizadorComponent },
+  { path: '**', redirectTo: 'home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- komikaFront/src/app/comics.service.ts
+++ komikaFront/src/app/comics.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { Comic } from './models/comics.model';
+import { Autor } from './models/autores.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ComicsService {
+
+  comics: Comic[];
+  autores: Autor[];
 
   constructor() { }
 }
