6544a9d5010296eba351ce88d5609ea0c570db9f
Alwar <alwaromoreno@gmail.com>
2020-06-04 17:43:05 +0200

    Modulo PDF ,visualizador implementado

--- komikaFront/src/app/app.module.ts
+++ komikaFront/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { PerfilComponent } from './perfil/perfil.component';
@@ -21,7 +23,8 @@ import { RegistroComponent } from './registro/registro.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    NgxExtendedPdfViewerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
