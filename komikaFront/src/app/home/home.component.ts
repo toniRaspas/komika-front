4856c2b4b1b10abf006d8ad6b37d9522f7c5bfa8
toniRaspas <txikiartvideo@gmail.com>
2020-06-08 17:11:02 +0200

    validacionAngular

--- komikaFront/src/app/home/home.component.ts
+++ komikaFront/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  login: FormGroup;
+  constructor() {
+    this.login = new FormGroup({
+      email: new FormControl('', [Validators.required,
+      Validators.pattern(/^\w+[\w-\.]*\@\w+((-\w+)|(\w*))\.[a-z]{2,3}$/)
+      ]),
+      password: new FormControl('', [Validators.required, Validators.pattern(/^[a-zA-Z]\w{8,30}$/)
+      ])
+    })
+  }
+
 
   ngOnInit(): void {
   }
-
+  onSubmit() { console.log(this.login.value) }
 }
--- komikaFront/src/app/registro/registro.component.ts
+++ komikaFront/src/app/registro/registro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registro.component.css']
 })
 export class RegistroComponent implements OnInit {
+  //creamos una variable del tipo FormGroup
+  registro: FormGroup;
 
-  constructor() { }
+  constructor() {
+
+    //inicializamos dichas variables
+    this.registro = new FormGroup({
+
+      nombre: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      usuario: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required,
+      Validators.pattern(/^\w+[\w-\.]*\@\w+((-\w+)|(\w*))\.[a-z]{2,3}$/)
+      ]),
+      password: new FormControl('', [Validators.required, Validators.pattern(/^[a-zA-Z]\w{8,30}$/)
+      ]),
+    })
+  }
 
   ngOnInit(): void {
   }
 
+  onSubmit() {
+    console.log(this.registro.value);
+
+  }
 }
