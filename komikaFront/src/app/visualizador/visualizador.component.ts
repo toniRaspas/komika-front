8f909cf46f61cc500359caa1584beade585ce961
toniRaspas <txikiartvideo@gmail.com>
2020-06-17 10:52:22 +0200

    para pull

--- komikaFront/src/app/visualizador/visualizador.component.ts
+++ komikaFront/src/app/visualizador/visualizador.component.ts
@@ -17,7 +17,7 @@ export class VisualizadorComponent implements OnInit {
 
 
 
-  pdfSrc = "../../assets/images/piano_rojo.pdf";
+  pdfSrc = "https://firebasestorage.googleapis.com/v0/b/app-komika.appspot.com/o/daredevil.pdf?alt=media&token=f9062563-48e0-4d56-aa54-c27adf98bcd7"
 
 
 
