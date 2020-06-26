4e3e6acda9ad52e05858ce6d5802f217c735a8e3
Alwar <alwaromoreno@gmail.com>
2020-06-26 09:56:04 +0200

    favicon

--- komikaFront/e2e/src/app.e2e-spec.ts
+++ komikaFront/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('komikaFront app is running!');
+    expect(page.getTitleText()).toEqual('Komika app is running!');
   });
 
   afterEach(async () => {
